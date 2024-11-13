7711da9a2bb7405244a95d7c9eb8d78382d07f94
Ryan Hanks <ryandalehanks@gmail.com>
2024-11-13 02:15:53 -0600

    license setup

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)]`;
+  } else if (license === 'Apache 2.0') {
+    return `[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-green.svg)]`;
+  } else {
+    return 'No License';
+  };
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if(license === 'MIT'){
+    return '(https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache 2.0'){
+    return '(https://opensource.org/licenses/Apache-2.0)';
+  } else {
+    return '';
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  return `${renderLicenseBadge(license) + renderLicenseLink(license)}`;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
